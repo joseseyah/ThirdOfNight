703715f1e16ef0ab781d7887d1b4ca83e88b3d3a
joseseyah <joseph.hayes003@gmail.com>
2025-01-01 13:45:43 +0000

    2.3.0

--- third2.0/Text/mosqueList.swift
+++ third2.0/Text/mosqueList.swift
@@ -7,4 +7,4 @@
 
 import Foundation
 
-let mosqueList = ["Manchester Isoc", "Cheadle Masjid", "AlFurqan Manchester","Wythenshawe Masjid", "Masjid Eumer", "Altrinham Hale Muslim Assoc.", "Glasgow central masjid", "Green-bank masjid Bristol", "Jami Masjid & islamic centre - Birmingham"]
+let mosqueList = ["Manchester Isoc", "Cheadle Masjid", "AlFurqan Manchester","Wythenshawe Masjid", "Masjid Eumer", "Altrinham Hale Muslim Assoc.", "Glasgow central masjid", "Green-bank masjid Bristol", "Jami Masjid & islamic centre - Birmingham", "Jamia Khizra Mosque - Bury", "Masjid Ar-Raheem"]

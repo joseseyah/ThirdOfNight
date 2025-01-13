b0e039baccef8f90fba689247d9a2a0c3c08f25d
M.waqar <waqar@codeclever.tech>
2025-01-13 23:34:46 +0500

    - cache implementation

--- third2.0/third2_0App.swift
+++ third2.0/third2_0App.swift
@@ -5,14 +5,13 @@ import UserNotifications
 import BackgroundTasks
 import AVFoundation
 import FirebaseFirestore
-
+import Network
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-        requestNotificationPermission()
-        configureAudioSession()
+        FirebaseHelper.shared.setupFireBase()
 
+        configureAudioSession()
         // Set up background fetch interval
         UIApplication.shared.setMinimumBackgroundFetchInterval(UIApplication.backgroundFetchIntervalMinimum)
 
@@ -23,17 +22,6 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 
         return true
     }
-
-    private func requestNotificationPermission() {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
-            if granted {
-                print("Notification permission granted.")
-            } else if let error = error {
-                print("Notification permission denied: \(error.localizedDescription ?? "Unknown error")")
-            }
-        }
-    }
-
     private func configureAudioSession() {
         do {
             try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default, options: [])
@@ -46,7 +34,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 
     // Handle background fetch for prayer times
     func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        updatePrayerTimes { result in
+        FirebaseHelper.shared.updatePrayerTimes { result in
             switch result {
             case .success:
                 completionHandler(.newData)
@@ -60,7 +48,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     private func handleBackgroundTask(task: BGAppRefreshTask) {
         scheduleBackgroundTask() // Reschedule for the next day
         
-        updatePrayerTimes { result in
+        FirebaseHelper.shared.updatePrayerTimes{ result in
             switch result {
             case .success:
                 task.setTaskCompleted(success: true)
@@ -81,62 +69,22 @@ class AppDelegate: NSObject, UIApplicationDelegate {
             print("Failed to schedule background task: \(error.localizedDescription)")
         }
     }
-
-    // Function to fetch and update prayer times
-    private func updatePrayerTimes(completion: @escaping (Result<Void, Error>) -> Void) {
-        let useMosqueTimetable = UserDefaults.standard.bool(forKey: "useMosqueTimetable")
-        let selectedMosque = UserDefaults.standard.string(forKey: "selectedMosque") ?? ""
-        let selectedCity = UserDefaults.standard.string(forKey: "selectedCity") ?? ""
-
-        let db = Firestore.firestore()
-
-        if useMosqueTimetable {
-            // Fetch prayer times for the selected mosque
-            let docRef = db.collection("Mosques").document(selectedMosque)
-            docRef.getDocument { document, error in
-                if let document = document, document.exists {
-                    if let prayerTimes = document.data()?["data"] as? [[String: String]] {
-                        // Save updated prayer times
-                        UserDefaults.standard.set(prayerTimes, forKey: "prayerTimes")
-                        completion(.success(()))
-                    } else {
-                        completion(.failure(NSError(domain: "No data found", code: 1, userInfo: nil)))
-                    }
-                } else {
-                    completion(.failure(error ?? NSError(domain: "Failed to fetch", code: 1, userInfo: nil)))
-                }
-            }
-        } else {
-            // Fetch prayer times for the selected city
-            let docRef = db.collection("prayerTimes").document(selectedCity)
-            docRef.getDocument { document, error in
-                if let document = document, document.exists {
-                    if let prayerTimes = document.data()?["prayerTimes"] as? [[String: Any]] {
-                        // Save updated prayer times
-                        UserDefaults.standard.set(prayerTimes, forKey: "prayerTimes")
-                        completion(.success(()))
-                    } else {
-                        completion(.failure(NSError(domain: "No data found", code: 1, userInfo: nil)))
-                    }
-                } else {
-                    completion(.failure(error ?? NSError(domain: "Failed to fetch", code: 1, userInfo: nil)))
-                }
-            }
-        }
-    }
 }
 
 @main
 struct third2_0App: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject var audioPlayerViewModel = AudioPlayerViewModel()
-
+    @StateObject private var networkMonitor = NetworkMonitor()
     @AppStorage("hasSeenOnboarding") private var hasSeenOnboarding: Bool = false
-
+    
     var body: some Scene {
         WindowGroup {
             if hasSeenOnboarding {
                 ContentView()
+                    .onChange(of: networkMonitor.isConnected) { isConnected in
+                        setupMode(isConnected: isConnected)
+                    }
                     .environmentObject(audioPlayerViewModel)
                     .onAppear {
                         scheduleBackgroundTask()
@@ -146,14 +94,41 @@ struct third2_0App: App {
             }
         }
     }
-
+    
     public func scheduleBackgroundTask() {
         if UIApplication.shared.backgroundRefreshStatus == .available {
             let appDelegate = UIApplication.shared.delegate as? AppDelegate
             appDelegate?.scheduleBackgroundTask()
         }
     }
+    private func setupMode(isConnected: Bool? = nil) {
+        if isConnected ?? false{
+            FirebaseHelper.shared.enableOnlineMode(completion: { error in
+                
+            })
+        }else{
+            FirebaseHelper.shared.enableOfflineMode(completion: { error in
+                
+            })
+        }
+    }
 }
-
-
-
+class NetworkMonitor: ObservableObject {
+    private let monitor = NWPathMonitor()
+    private let queue = DispatchQueue(label: "NetworkMonitor")
+    
+    @Published var isConnected: Bool = false
+    
+    init() {
+        monitor.pathUpdateHandler = { [weak self] path in
+            DispatchQueue.main.async {
+                self?.isConnected = path.status == .satisfied
+            }
+        }
+        monitor.start(queue: queue)
+    }
+    
+    deinit {
+        monitor.cancel()
+    }
+}

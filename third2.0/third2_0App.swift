87a295aa2aef15a4b7c267bf90020c68777234ca
M.waqar <>
2025-01-15 23:47:03 +0500

    - date issue fixed

--- third2.0/third2_0App.swift
+++ third2.0/third2_0App.swift
@@ -113,22 +113,3 @@ struct third2_0App: App {
         }
     }
 }
-class NetworkMonitor: ObservableObject {
-    private let monitor = NWPathMonitor()
-    private let queue = DispatchQueue(label: "NetworkMonitor")
-    
-    @Published var isConnected: Bool = false
-    
-    init() {
-        monitor.pathUpdateHandler = { [weak self] path in
-            DispatchQueue.main.async {
-                self?.isConnected = path.status == .satisfied
-            }
-        }
-        monitor.start(queue: queue)
-    }
-    
-    deinit {
-        monitor.cancel()
-    }
-}
